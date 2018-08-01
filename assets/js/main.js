787d9d5704513cdf1f723efa2fc3a069a6dc70c7
dustyjay <janetarisah01@gmail.com>
2018-08-01 16:35:14 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+    $hide_password = $('i.hide-password');
+
+    $hide_password.click(function(){
+        $this = $(this);
+        $hide_input = $this.siblings('input');
+        if($this.hasClass('fa-eye')){
+            $this.removeClass('fa-eye');
+            $this.addClass('fa-eye-slash');
+        }
+        else{
+            $this.addClass('fa-eye');
+            $this.removeClass('fa-eye-slash');
+        }
+        if($hide_input.attr('type') == "text"){
+            $hide_input.attr('type','password');
+        }
+        else{
+            $hide_input.attr('type','text');
+        }
+    })
+});
