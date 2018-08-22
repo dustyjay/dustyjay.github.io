c4cca9bfbb4ae66d7d0509368f146d214c098336
dustyjay <janetarisah01@gmail.com>
2018-08-22 09:31:45 +0100

    made fixes on the slide texts

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,8 @@ $(function(){
 
     $(".slides").slick({
         arrows: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
+        // autoplay: true,
+        // autoplaySpeed: 5000,
         prevArrow: '<span class="prev-arrow"><img src="assets/img/left-arrow.svg"/></span>',
         nextArrow: '<span class="next-arrow"><img src="assets/img/right-arrow.svg"/></span>',
         responsive: [
